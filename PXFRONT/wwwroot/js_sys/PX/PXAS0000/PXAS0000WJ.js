ef9ccdb838e034ebf1cb994a47b3cb43713d68da
a-bunazawa <a.bunazawa@primecast.co.jp>
2018-01-30 13:11:43 +0900

    足りないｊｓ追加 jsのパス指定変換が不足していたところを追加

--- PXFRONT/wwwroot/js_sys/PX/PXAS0000/PXAS0000WJ.js
+++ PXFRONT/wwwroot/js_sys/PX/PXAS0000/PXAS0000WJ.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     SetSysDB();
     resizeWindow();
 
-    //Ladda.bind('.button-demo button', { timeout: true });
+    Ladda.bind('.button-demo button', { timeout: true });
 
     //レイアウトオプションの日本語化
-    LocalLayoutOption();
+    //LocalLayoutOption();
 
     $("#email").focus();
     $("input").keypress(function (e) {
@@ -36,51 +36,53 @@ function resizeWindow() {
 
 function Login() {
     //var url = localStorage.getItem("LOGINEXT_WMSAPI_URL") + "api/LNAS0001/Login";
-    //var data = {};
-    //data.Id = $("#email").val();
-    //data.Pass = $("#password").val();
-    //data.DB = localStorage.getItem("LOGINEXT_WMSAPI_SYSTEMDB");
+    var url = "https://kanda/KANDANET/API/api/LNAS0001/Login";
 
-    //$.ajax(url, { type: "POST", data: data }).then(function (r) {
-    //    localStorage.setItem("LOGINEXT_WMSAPI_USERNM", r.Name);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_COPCD", r.COPCD);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_SYSID", r.SYSID);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_MENUID", r.MENUID);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_USERDBNM", r.USERDBNM);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRNM", r.USERDBSVRNM);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRIP", r.USERDBSVRIP);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRUR", r.USERDBSVRUR);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRPW", r.USERDBSVRPW);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_INIGRPCD", r.INIGRPCD);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_INIDPTCD", r.INIDPTCD);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_INIWHSCD", r.INIWHSCD);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_INICMPCD", r.INICMPCD);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_INICSTCD", r.INICSTCD);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_INISHPCD", r.INISHPCD);
-    //    localStorage.setItem("LOGINEXT_WMSAPI_USERID", r.Id);
+    var data = {};
+    data.Id = $("#email").val();
+    data.Pass = $("#password").val();
+    data.DB = localStorage.getItem("LOGINEXT_WMSAPI_SYSTEMDB");
 
-    //    if (r.SendUrl != "") {
-    //        window.location.href = r.SendUrl;
-    //    } else {
-    //        createDialog("dialog", "1,0,ログイン画面,ALM," + r.ErrorMsg + ",ＯＫ,ALM000-01,OK");
-    //        $("#dialog").dialog("open");
-    //        $("#dialog").focus();
-    //        Ladda.stopAll();
-    //    }
-    //}, function (e) {
-    //    createDialog("dialog", "1,0,ログイン画面,ALM,error: " + e + ",ＯＫ,ALM000-01,OK");
-    //    $("#dialog").dialog("open");
-    //    Ladda.stopAll();
-    //});
+    $.ajax(url, { type: "POST", data: data }).then(function (r) {
+        localStorage.setItem("LOGINEXT_WMSAPI_USERNM", r.Name);
+        localStorage.setItem("LOGINEXT_WMSAPI_COPCD", r.COPCD);
+        localStorage.setItem("LOGINEXT_WMSAPI_SYSID", r.SYSID);
+        localStorage.setItem("LOGINEXT_WMSAPI_MENUID", r.MENUID);
+        localStorage.setItem("LOGINEXT_WMSAPI_USERDBNM", r.USERDBNM);
+        localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRNM", r.USERDBSVRNM);
+        localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRIP", r.USERDBSVRIP);
+        localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRUR", r.USERDBSVRUR);
+        localStorage.setItem("LOGINEXT_WMSAPI_USERDBSVRPW", r.USERDBSVRPW);
+        localStorage.setItem("LOGINEXT_WMSAPI_INIGRPCD", r.INIGRPCD);
+        localStorage.setItem("LOGINEXT_WMSAPI_INIDPTCD", r.INIDPTCD);
+        localStorage.setItem("LOGINEXT_WMSAPI_INIWHSCD", r.INIWHSCD);
+        localStorage.setItem("LOGINEXT_WMSAPI_INICMPCD", r.INICMPCD);
+        localStorage.setItem("LOGINEXT_WMSAPI_INICSTCD", r.INICSTCD);
+        localStorage.setItem("LOGINEXT_WMSAPI_INISHPCD", r.INISHPCD);
+        localStorage.setItem("LOGINEXT_WMSAPI_USERID", r.Id);
+
+        if (r.SendUrl != "") {
+            window.location.href = r.SendUrl;
+        } else {
+            createDialog("dialog", "1,0,ログイン画面,ALM," + r.ErrorMsg + ",ＯＫ,ALM000-01,OK");
+            $("#dialog").dialog("open");
+            $("#dialog").focus();
+            Ladda.stopAll();
+        }
+    }, function (e) {
+        createDialog("dialog", "1,0,ログイン画面,ALM,error: " + e + ",ＯＫ,ALM000-01,OK");
+        $("#dialog").dialog("open");
+        Ladda.stopAll();
+    });
 
     window.location.href = "https://" + window.location.host + "/PXAS/PXAS0010/PXAS0010VW";
 
-    //var error = "2,0,アラーム,ALM,システム利用期限が間もなく終了します。,ＯＫ,ALM000-01_OK,OK,キャンセル,ALM000-01_NG,NG";
-    //var dialog = $("#dialog");
-    //if (dialog != null) {
-    //    createDialog("dialog", error);
-    //    dialog.dialog("open");
-    //}
+    var error = "2,0,アラーム,ALM,システム利用期限が間もなく終了します。,ＯＫ,ALM000-01_OK,OK,キャンセル,ALM000-01_NG,NG";
+    var dialog = $("#dialog");
+    if (dialog != null) {
+        createDialog("dialog", error);
+        dialog.dialog("open");
+    }
 
     return false;
 }
